aa89097728607054ce744ecf337560c95dc84098
jhuniorhuamancuri123 <jhuniorhuamancuri123@gmail.com>
2026-06-04 22:33:45 -0500

    fix: pixel meta y analytics separados

--- app/layout.js
+++ app/layout.js
@@ -52,33 +52,33 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-FQ6GY0YT7S"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-        <Script id="facebook-pixel" strategy="afterInteractive">
-  {`
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '3915121135274598');
-    fbq('track', 'PageView');
-  `}
-</Script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-FQ6GY0YT7S');
-          `}
-        </Script>
-      </head>
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-FQ6GY0YT7S"
+    strategy="afterInteractive"
+  />
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-FQ6GY0YT7S');
+    `}
+  </Script>
+  <Script id="facebook-pixel" strategy="afterInteractive">
+    {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '3915121135274598');
+      fbq('track', 'PageView');
+    `}
+  </Script>
+</head>
       <body className={`${playfair.variable} ${cormorant.variable}`}>
         <ToastProvider>
           <CarritoProvider>
